c67c5b63e34f8e80209f88a7358fd4395c73c643
abodzsar29 <bbodzsar29@gmail.com>
2023-10-11 22:28:43 +0100

    Finalised Comments

--- Additional Files/Python Script/main.py
+++ Additional Files/Python Script/main.py
@@ -1,9 +1,13 @@
+# The following python code sets a custom header and sends a request to the web server to retrieve index.php. The IP address was obtained from an Ubuntu OS running Nginx on a Linode web server
+# The purpose of this was to test whether the pre-set custom headers are correctly blocked
+
+
 import requests
 from bs4 import BeautifulSoup
 
 url = "http://213.52.128.13/"
-headers = {'User-Agent':"BlockedPythonUA"}
+headers = {'User-Agent':"BlockedPythonUA"}  # Custom header
 
-r = requests.get(url, headers=headers)
-doc = BeautifulSoup(r.text, "html.parser")
+r = requests.get(url, headers=headers)  # Obtaining index.php
+doc = BeautifulSoup(r.text, "html.parser")  # Storing the HTML response
 print(doc)
--- Additional Files/Software Installation/readme.txt
+++
@@ -1,3 +0,0 @@
-The installation of all of the utilities are described in Chapter 6 of the report. 
-
-To find commands which to execute order for easier installation, look at the stepbystepinstallation.txt file
